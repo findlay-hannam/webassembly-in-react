9f548c2dc17132805c23244f1ee2dae7bee0309a
findlay-hannam <f.hannam@elsevier.com>
2020-10-30 17:19:28 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import waApi from './wa-api';
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState(5);
+  const [result, setResult] = useState();
+  waApi.then(console.log);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        The factorial of
+        <input value={value} onChange={evt => setValue(evt.target.value)} />
+        is {result}
+      </p>
+      <button onClick={async () => setResult((await waApi).exports.factorial(value))}>
+        Calculate
+      </button>
     </div>
   );
 }
-
-export default App;
